2f6f8d10611cecdedc47c6e5c0ff2a290315ba97
azgel <azgel@DESKTOP-2K9D8KE>
2024-03-28 08:11:58 +0300

    what

---
+++ profile_for_signed_in_user.js
@@ -0,0 +1,6 @@
+const name = document.querySelector(".name");
+
+function openSignedInProfilePhotos() {
+    alert("button clicked");
+    window.location.replace("profile_photos_for_signed_in_user.html");
+}
