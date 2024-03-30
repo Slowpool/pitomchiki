6565dc27f77be9e4c2e39906de6e2794fdae90ee
azgel <azgel@DESKTOP-2K9D8KE>
2024-03-30 15:55:37 +0300

    something was added a long time ago

--- profile_for_signed_in_user.js
+++ profile_for_signed_in_user.js
@@ -1,6 +1,17 @@
 const name = document.querySelector(".name");
 
 function openSignedInProfilePhotos() {
-    alert("button clicked");
     window.location.replace("profile_photos_for_signed_in_user.html");
+}
+
+function openSignedInProfile() {
+    window.location.replace("profile_for_signed_in_user.html");
+}
+
+function openSignedInProfileFeatures() {
+    window.location.replace("profile_for_signed_in_user.html");
+}
+
+function openSignedInProfileReviews() {
+    window.location.replace("profile_for_signed_in_user.html");
 }
